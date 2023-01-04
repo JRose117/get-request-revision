e65943f6c1b75fda4cd3b1562bef8fd93446fff1
JRose117 <joerose123@hotmail.co.uk>
2023-01-04 16:42:18 +0000

    all drinks and drink single working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useEffect } from 'react'
 import axios from 'axios'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AllDrinks from './components/AllDrinks'
+import DrinkSingle from './components/DrinkSingle'
 const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
 
-  return <h1>Hello World</h1>
+  return (
+    <div className="site-wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllDrinks />} />
+          <Route path="/drink/:id" element={<DrinkSingle />} />
+
+          {/* <Route path='/anime/:animeId' element={<OneAnime />}/> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.scss'
 import App from './App'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
 
 createRoot(document.getElementById('root')).render(<App />)
